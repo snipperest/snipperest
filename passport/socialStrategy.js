ea75292db0843717ad734c9bfcc1100201a09ede
Nira <niralonso@gmail.com>
2019-02-14 10:38:47 +0100

    .

--- passport/socialStrategy.js
+++ passport/socialStrategy.js
@@ -1,39 +1,39 @@
-const passport      = require('passport');
+require("dotenv").config();
+const passport = require('passport');
 const GithubStrategy = require('passport-github2').Strategy;
-const User          = require('../models/User');
-const bcrypt        = require('bcrypt'); 
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 
 passport.use(new GithubStrategy({
-    // passReqToCallback: true,
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  }, (accessToken, refreshToken, profile ,done) => {
-  
-    
-    User.findOne({GitHubId: profile.id })
+  // passReqToCallback: true,
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: `${process.env.HOST}/auth/github/callback`
+}, (accessToken, refreshToken, profile, done) => {
+  User.findOne({ GitHubId: profile.id })
     .then(user => {
-      
+
       if (user) {
         return done(null, user);
       }
-  
+
       const newUser = new User({
         GitHubId: profile.id,
-        // photo: profile.photo,
-        username : profile.username
+        //photo: profile.photo,
+        username: profile.username
       });
-  
+
       newUser.save()
-      .then(user => {
-        done(null, newUser);
-      })
+        .then(user => {
+          done(null, user);
+        })
     })
     .catch(error => {
       done(error)
     })
-  
-  }));
-  
+
+}));
+
 
 
